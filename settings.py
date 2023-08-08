e06dcb05cb7b572a9c9d17a212ba2407b4acd034
Anindya Arman Putri <93259436+anindyaarman@users.noreply.github.com>
2023-08-08 10:06:12 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -29,7 +29,7 @@ LANGUAGE_CODE = 'en'
 REAL_WORLD_CURRENCY_CODE = 'USD'
 USE_POINTS = True
 POINTS_CUSTOM_NAME = 'tokens'
-Debug = False
+DEBUG = False
 
 ADMIN_USERNAME = 'admin'
 # for security, best to set admin password in an environment variable
